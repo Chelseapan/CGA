72dac565aa6d321bfce01302e3670598b4ede6c4
Chelsea Pan <43884605+Chelseapan@users.noreply.github.com>
2021-06-21 21:58:04 +0800

    Delete 比较基因组分析 directory

--- 比较基因组分析/bars.sh
+++
@@ -1,9 +0,0 @@
-#bars
-
-!showlegend	1
-!grid	1
-!gridlabel	1
-!groups	JS,HN,ZJ,YN,XJ,SD,QH,NX,NMG,JL,GS,GD,FJ,CQ,BJ,Others
-!colors	red,lightgreen,orange,brown,pink,lightblue,olive,gold,silver,indigo,beige,khaki,yellow,aqua,lavender,black
-!title	Province
-
